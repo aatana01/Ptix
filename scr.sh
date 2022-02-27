525c1e5393121818c92490f96d7361e4c6a5c25b
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-02-27 16:00:28 +0200

    Add files via upload

--- scr.sh
+++ scr.sh
@@ -4,20 +4,22 @@ for (( i=0 ; i<$1 ; i++ ));
 do
     if [ $i=0 ]; then
 	chmod +x scr_master.sh
-	yes Y|./scr_master.sh
-	variable=`cat file |  grep "docker swarm join --token"`
+	cat scr_master.sh
+	#yes Y|./scr_master.sh
+	#variable=`cat file |  grep "docker swarm join --token"`
 else
-	yes Y|ssh node$i
-	git clone https://github.com/aatana01/Ptix.git
-	cd Ptix
-	chmod +x scr_work.sh
-	yes Y|./scr_work.sh
-	$variable
+	echo "Worker now"
+	#yes Y|ssh node$i
+	#git clone https://github.com/aatana01/Ptix.git
+	#cd Ptix
+	#chmod +x scr_work.sh
+	#yes Y|./scr_work.sh
+	#$variable
 fi
 done
-yes Y|ssh node0
-cd Ptix
-sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork
-python3 scripts/init_social_graph.py --graph=socfb-Reed98
-cd wrk2
-make
+#yes Y|ssh node0
+#cd Ptix
+#sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork
+#python3 scripts/init_social_graph.py --graph=socfb-Reed98
+#cd wrk2
+#make
