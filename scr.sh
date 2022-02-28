a441da3e72b9654599b8b4d7797f6a63cc0562be
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-02-28 23:35:42 +0200

    Add files via upload

--- scr.sh
+++ scr.sh
@@ -20,9 +20,16 @@ EOT
 	
 fi
 done
-#yes Y|ssh node0
+sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork
+#ssh -o "StrictHostKeyChecking no"  -A node$1<<EOT
+#	git clone https://github.com/aatana01/Ptix.git
+#	cd Ptix
+#	chmod +x scr_work.sh
+#	yes Y|./scr_work.sh
+#	sudo$variable
+#EOT
 #cd Ptix
-#sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork
-#python3 scripts/init_social_graph.py --graph=socfb-Reed98
-#cd wrk2
-#make
+
+python3 scripts/init_social_graph.py --graph=socfb-Reed98
+cd wrk2
+make
