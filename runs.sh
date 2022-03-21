d80e4b9d7faec041871697e0e190cb100e1076a2
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-03-21 21:20:05 +0200

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 chmod +x sudo.sh
 for (( c=100 ; c<500 ; c+=100 ));
+	o=$1
 	echo $c>>file
-	./wrk -D exp -t 2 -c 2 -d 100 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $c|./sudo.sh
+	./wrk2/wrk -D exp -t 2 -c 2 -d 100 -L -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $c|./sudo.sh $o
 
 done
 #do this to all nodes that have containers: to run first sudo nano /etc/default/grub change default to intel_pstate=disable then cntrl x yes and enter and run sudo update-grub2 after that reboot(when i reboot cant connect,have to restart moba) and run https://github.com/hvolos/mcperf 
