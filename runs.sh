ee7d015aeb95da4bf0981ad0711813141563c7dc
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-03-21 21:27:48 +0200

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 chmod +x sudo.sh
 for (( c=100 ; c<500 ; c+=100 ));
+do
 	o=$1
 	echo $c>>file
 	./wrk2/wrk -D exp -t 2 -c 2 -d 100 -L -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $c|./sudo.sh $o
