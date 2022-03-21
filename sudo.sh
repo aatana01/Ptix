a180b375a9f490147485a8abe1c3965d6ee663fd
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-03-21 21:20:40 +0200

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 for (( i=0 ; i<$1 ; i++ ));
 do
-        sleep 2
         echo $i>>file
-        ssh  node$i "sudo docker stats --no-stream;sleep 1;sudo docker stats --no-stream;sleep 1;sudo docker stats --no-stream;sleep 1;sudo docker stats --no-stream;sleep 1;">>file
+        ssh  node$i "sudo docker stats --no-stream;sleep 1;sudo docker stats --no-stream;sleep 1;">>file
 
 
 done
