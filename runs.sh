0908315f04ba07c2ec983e843f3d4e526aab136a
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-03-22 15:46:49 +0200

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod +x sudo.sh
+o=$1
 for (( c=0 ; c<$1 ; c++ )); 
 do
 		python3 ./profiler.py -n node$c start
@@ -7,7 +8,7 @@ done
 for (( c=100 ; c<500 ; c+=100 ));
 do
 	echo $c>>file
-	./wrk -D exp -t 2 -c 2 -d 100 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $c|./sudo.sh
+	./wrk -D exp -t 2 -c 2 -d 100 -L -s ./scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $c>>file1|./sudo.sh $o
 
 done
 for (( c=0 ; c<$1 ; c++ )); 
