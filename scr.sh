c20c39b92b8e706d01afd2c87d77a26c4477b2df
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-04-14 19:17:25 +0300

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -4,9 +4,13 @@ for (( i=0 ; i<$1 ; i++ ));
 do
 	
     if [ $i -eq 0 ]; then
+    	ssh node$i<<EOT
+	git clone https://github.com/aatana01/Ptix.git
+	cd Ptix
 	chmod +x scr_master.sh
 	yes Y|./scr_master.sh
 	variable=`cat file |  grep "docker swarm join --token"`
+EOT
 else
 	
 	ssh node$i<<EOT
@@ -20,19 +24,17 @@ EOT
 	
 fi
 done
-sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork
+ssh  node0 "sudo docker stack deploy --compose-file=docker-compose-swarm.yml SocialNetwork"
 
-ssh  node$i "git clone https://github.com/aatana01/Ptix.git;
-cd Ptix;
-chmod +x scr_work.sh;
-yes Y|./scr_work.sh;
+git clone https://github.com/aatana01/Ptix.git
+cd Ptix
+chmod +x scr_work.sh
+yes Y|./scr_work.sh
 sudo$variable;
-git clone https://github.com/hvolos/profiler.git;
-chmod +x pstate.sh;
-chmod +x turbo_rm.sh"
-#python3 scripts/init_social_graph.py --graph=socfb-Reed98;
-#cd wrk2;
-#make;
+#git clone https://github.com/hvolos/profiler.git;
+python3 scripts/init_social_graph.py --graph=socfb-Reed98;
+cd wrk2;
+make;
 
 
 
