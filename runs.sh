34a0f1534ae88ca1f42fa690dc75880b00f8a021
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-05-20 12:23:47 +0300

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -7,7 +7,7 @@ do
                 python3 ./profiler/profiler.py -n node$c start
 done
         
-        ./wrk2/wrk -D exp -t 2 -c 2 -d 60 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
+        ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
 
 
 for (( c=0 ; c<$1 ; c++ ));
