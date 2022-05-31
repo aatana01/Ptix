903518e9837a2de0c05b97cbb46bd9675ed31472
aatana01 <49650495+aatana01@users.noreply.github.com>
2022-06-01 02:29:30 +0300

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -4,19 +4,19 @@
 o=$1
 for (( c=0 ; c<$1 ; c++ ));
 do
-                python3 ./profiler/profiler.py -n node$c start
-done
+ #               python3 ./profiler/profiler.py -n node$c start
+#done
         
         ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
 
 
-for (( c=0 ; c<$1 ; c++ ));
-do
-                python3 ./profiler/profiler.py -n node$c stop
-done
-for (( c=0 ; c<$1 ; c++ ));
-do
-               python3 ./profiler/profiler.py -n node$c report -d /tmp/data/node$c
+#for (( c=0 ; c<$1 ; c++ ));
+#do
+#                python3 ./profiler/profiler.py -n node$c stop
+#done
+#for (( c=0 ; c<$1 ; c++ ));
+#do
+#               python3 ./profiler/profiler.py -n node$c report -d /tmp/data/node$c
 done
 
 
